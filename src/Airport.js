b4957305e770c1e2b0355ad5453aa6d264dd2d17
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-30 16:42:15 +0100

    Extracted 'Weather' into it's own class

--- src/Airport.js
+++ src/Airport.js
@@ -1,6 +1,7 @@
 'use strict';
 
-function Airport () {
+function Airport (weather) {
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
   this._hanger = [];
 };
 
@@ -9,19 +10,15 @@ Airport.prototype.planes = function () {
 };
 
 Airport.prototype.clearForLanding = function (plane) {
-  if(this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot land during storm');
   }
   this._hanger.push(plane);
 };
 
 Airport.prototype.clearForTakeOff = function(plane) {
-  if(this.isStormy()) {
+  if(this._weather.isStormy()) {
     throw new Error('cannot takeoff during storm');
   }
   this._hanger = [];
-};
-
-Airport.prototype.isStormy = function() {
-  return false;
 };
