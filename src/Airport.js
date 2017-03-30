981227d5a4b4602487d363be8de4d79fdf05b050
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-30 15:37:02 +0100

    Take off & landing complete

---
+++ src/Airport.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function Airport () {
+  this._hanger = [];
+};
+
+Airport.prototype.planes = function () {
+  return this._hanger
+};
+
+Airport.prototype.clearForLanding = function (plane) {
+  this._hanger.push(plane);
+};
+
+Airport.prototype.clearForTakeOff = function(plane) {
+  this._hanger = [];
+};
