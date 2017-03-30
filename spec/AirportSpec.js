3e4ab58e32b8c70391f3333acc3f582e29fd400a
Daniel Clissold <dan@Daniels-MacBook-Pro.local>
2017-03-30 15:57:22 +0100

    Takeoff & landing prohibited during storms

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -23,4 +23,21 @@ describe('Airport', function(){
     airport.clearForTakeOff(plane);
     expect(airport.planes()).toEqual([]);
   });
+
+  it('can check for stormy conditions', function() {
+    expect(airport.isStormy()).toBeFalsy();
+  });
+
+  describe('under stormy conditions', function() {
+
+    it('does not clear planes for takeoff', function() {
+      spyOn(airport, 'isStormy').and.returnValue(true);
+      expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+    });
+
+    it('does not clear planes for landing', function() {
+      spyOn(airport, 'isStormy').and.returnValue(true);
+      expect(function() { airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
+    });
+  });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -9,9 +9,19 @@ Airport.prototype.planes = function () {
 };
 
 Airport.prototype.clearForLanding = function (plane) {
+  if(this.isStormy()) {
+    throw new Error('cannot land during storm');
+  }
   this._hanger.push(plane);
 };
 
 Airport.prototype.clearForTakeOff = function(plane) {
+  if(this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
   this._hanger = [];
+};
+
+Airport.prototype.isStormy = function() {
+  return false;
 };
